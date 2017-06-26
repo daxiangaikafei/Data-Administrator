e179ff859a8fe42ba8651c0a873425709d04d961
WinterFete <wangqingzhu1025@126.com>
2017-06-26 10:45:55 +0800

    email服务器路由

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -2,6 +2,7 @@ import * as Router from "koa-router";
 // let routers_qbii:Router = require("./../modular/qbii/index");
 // let routers_good:Router = require("./../modular/good/index");
 // let routers_sys :Router = require("./../modular/sys/index");
+let email: Router = require("./../modular/email/index");
 
 
 let routers_syss :Router = require("./../sys/api/auth/index");
@@ -10,7 +11,8 @@ let routes:Router[]= [
 	// routers_qbii,
 	// routers_good,
 	// routers_sys,
-	routers_syss
+	routers_syss,
+	email
 ];
 //module.exports = router
 module.exports = routes;
